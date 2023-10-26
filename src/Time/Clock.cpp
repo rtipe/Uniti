3626825e139abc84012b3a045a17996b637939da
SimonVermeulen <simon.vermeulen@epitech.eu>
2023-10-26 17:40:12 +0200

    [Feat](Clock): creation of Clock

--- src/Time/Clock.cpp
+++ src/Time/Clock.cpp
@@ -5,4 +5,54 @@
 #include "Clock.hpp"
 
 namespace Uniti {
+    Clock::Clock()
+    {
+        _start = std::chrono::high_resolution_clock::now();
+    }
+
+    void Clock::restart()
+    {
+        _start  = std::chrono::high_resolution_clock::now();
+        _paused = false;
+    }
+
+    long long Clock::getSeconds() const
+    {
+        auto current_time = (!_paused) ? std::chrono::high_resolution_clock::now() : _pause;
+        auto duration     = std::chrono::duration_cast<std::chrono::seconds>(current_time - _start);
+        return duration.count();
+    }
+
+    long long Clock::getMilliSeconds() const
+    {
+        auto current_time = (!_paused) ? std::chrono::high_resolution_clock::now() : _pause;
+        auto duration     = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - _start);
+        return duration.count();
+    }
+
+    long long Clock::getMicroSeconds() const
+    {
+        auto current_time = (!_paused) ? std::chrono::high_resolution_clock::now() : _pause;
+        auto duration     = std::chrono::duration_cast<std::chrono::microseconds>(current_time - _start);
+        return duration.count();
+    }
+
+    void Clock::pause()
+    {
+        _paused = true;
+        _pause  = std::chrono::high_resolution_clock::now();
+    }
+
+    void Clock::start()
+    {
+        if (_paused) {
+            auto current_time = std::chrono::high_resolution_clock::now();
+            auto pause_duration =
+                    std::chrono::duration_cast<std::chrono::nanoseconds>(current_time - _pause);
+            _start += pause_duration;
+            _paused = false;
+        } else {
+            this->restart();
+        }
+    }
 } // Uniti
