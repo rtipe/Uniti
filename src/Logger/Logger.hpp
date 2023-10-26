773fd24244171b53cbeedabc443f1b8450a0565b
IKEA.luddes <clement.pecas33@gmail.com>
2023-10-26 16:42:08 +0200

    [Feat] (Logger) : add Logger

--- src/Logger/Logger.hpp
+++ src/Logger/Logger.hpp
@@ -17,16 +17,31 @@ namespace Uniti {
             DANGER,
             INFO
         };
+        ~Logger();
         using eventLoggerFunction = std::function<std::string(STATUS status, const std::string &text)>;
         static void Log(const std::string &text);
         static void Warn(const std::string &text);
         static void Danger(const std::string &text);
         static void Info(const std::string &text);
+        static void Print(STATUS status, const std::string &text);
         Logger &operator<<(const std::string &text);
+        Logger &operator<<(const char *text);
         Logger &operator<<(const int &text);
         Logger &operator<<(const float &text);
         Logger &operator<<(const long &text);
         Logger &operator<<(const Json::Value &text);
-        void addEventListener(eventLoggerFunction function);
+        static Logger Log();
+        static Logger Warn();
+        static Logger Danger();
+        static Logger Info();
+        static void addEventListener(const std::string &name, eventLoggerFunction function);
+        static void changePath(const std::string &path);
+        static std::string &getPath();
+    private:
+        Logger(STATUS status);
+        static std::map<std::string, eventLoggerFunction> _events;
+        static std::string _path;
+        std::stringstream _buffer;
+        STATUS _status;
     };
 }
