c7cef7a4236a6cb2973774fe9edbe26aad7cc131
swann <swann.lagoute@epitech.eu>
2023-11-05 17:26:50 +0100

    [Doc](plugin/render): add Window.hpp

--- Plugins/Render/SFML/Window.hpp
+++ Plugins/Render/SFML/Window.hpp
@@ -10,24 +10,50 @@
 #include "Input.hpp"
 #include "Mouse.hpp"
 
+/** @brief Window class responsible for managing the application window and user input.
+*/
 class Window : public IWindow {
 public:
+    /** @brief Constructor for the Window class.
+    */
     Window();
 
+    /** @brief Check if the window is open.
+        @return bool: True if the window is open, otherwise false.
+    */
     bool isOpen() const override;
 
+    /** @brief Close the application window.
+    */
     void close() override;
 
+    /** @brief Display the contents of the window.
+    */
     void display() override;
 
+    /** @brief Create the application window based on the provided configuration.
+        @param Json::Value &value: JSON configuration data for window creation.
+    */
     void create(const Json::Value &value) override;
 
+    /** @brief Print a drawable object to the window.
+        @param IPrintable &printable: The object to be drawn to the window.
+    */
     void print(IPrintable &printable) override;
 
+    /** @brief Get a reference to the input system for user input.
+        @return IInput&: Reference to the input system.
+    */
     IInput &getInput() override;
 
+    /** @brief Get a reference to the mouse input system.
+        @return IMouse&: Reference to the mouse input system.
+    */
     IMouse &getMouse() override;
 
+    /** @brief Get a reference to the underlying SFML render window.
+        @return sf::RenderWindow&: Reference to the SFML render window.
+    */
     sf::RenderWindow &getRenderWindow();
 
 private:
@@ -38,3 +64,4 @@ private:
     Input _input;
     Mouse _mouse;
 };
+
