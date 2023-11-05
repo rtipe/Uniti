5612cd3fe2ca67e2fd84713f375751c1871fa9eb
swann <swann.lagoute@epitech.eu>
2023-11-05 17:24:19 +0100

    [Doc](plugin/render): add REnderPlugin.hpp

--- Plugins/Render/SFML/RenderPlugin.hpp
+++ Plugins/Render/SFML/RenderPlugin.hpp
@@ -8,42 +8,89 @@
 #include "IRenderPlugin.hpp"
 #include "Window.hpp"
 
+/** @brief RenderPlugin class responsible for managing rendering functionality.
+*/
 class RenderPlugin : public IRenderPlugin {
 public:
+    /** @brief Constructor for the RenderPlugin class.
+        @param Uniti::Core &core: Reference to the core of the application.
+    */
     RenderPlugin(Uniti::Core &core);
 
+    /** @brief Get a reference to the core of the application.
+        @return Uniti::Core&: Reference to the core.
+    */
     Uniti::Core &getCore();
 
+    /** @brief Get a constant reference to the window.
+        @return const IWindow&: Constant reference to the window.
+    */
     const IWindow &getWindow() const;
 
+    /** @brief Get a reference to the window.
+        @return IWindow&: Reference to the window.
+    */
     IWindow &getWindow();
 
+    /** @brief Perform actions when the plugin is awakened.
+        Typically used for initialization.
+    */
     void awake(const Json::Value &value);
 
+    /** @brief Perform actions before the start phase.
+    */
     void preStart();
 
+    /** @brief Perform actions during the start phase.
+    */
     void start();
 
+    /** @brief Perform actions after the start phase.
+    */
     void postStart();
 
+    /** @brief Perform actions before the update phase.
+    */
     void preUpdate();
 
+    /** @brief Perform actions during the update phase.
+    */
     void update();
 
+    /** @brief Perform actions after the update phase.
+    */
     void postUpdate();
 
+    /** @brief Perform actions before the end phase.
+    */
     void preEnd();
 
+    /** @brief Perform actions during the end phase.
+    */
     void end();
 
+    /** @brief Perform actions after the end phase.
+    */
     void postEnd();
 
+    /** @brief Get a constant reference to the clock.
+        @return const Uniti::Clock&: Constant reference to the clock.
+    */
     const Uniti::Clock &getClock() const;
 
+    /** @brief Get a reference to the clock.
+        @return Uniti::Clock&: Reference to the clock.
+    */
     Uniti::Clock &getClock();
 
+    /** @brief Get a constant reference to the event manager.
+        @return const Uniti::Event&: Constant reference to the event manager.
+    */
     const Uniti::Event &getEvent() const;
 
+    /** @brief Get a reference to the event manager.
+        @return Uniti::Event&: Reference to the event manager.
+    */
     Uniti::Event &getEvent();
 
 private:
