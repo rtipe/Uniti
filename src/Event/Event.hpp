a9bf50b822b4be0efe080350be46478726ab8fac
Badi <vincent.ballandi@epitech.eu>
2023-11-05 19:42:20 +0100

    [Doc] (event) : add doc event

--- src/Event/Event.hpp
+++ src/Event/Event.hpp
@@ -12,10 +12,23 @@ namespace Uniti {
     using eventFunction = std::function<void(const Json::Value &value)>;
     class Event {
     public:
+        /**
+         * @brief Construct a new Event object
+         * @param name
+         * @param function
+         */
         std::size_t addEvent(const std::string &name, eventFunction function);
+        /**
+         * @brief Destroy the Event object
+         * @param name
+         */
         void removeEvent(const std::string &name);
+        /**
+         * @brief Destroy the Event object
+         * @param name
+         * @param id
+         */
         void removeEvent(const std::string &name, std::size_t id);
-
         virtual void emitEvent(const std::string &name, const Json::Value &value, Logger &logger);
     private:
         std::map<std::string, std::vector<eventFunction>> _events;
