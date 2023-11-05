0c97530ec02dc8ffba3da3a858f6f803cd229284
swann <swann.lagoute@epitech.eu>
2023-11-05 15:29:51 +0100

    [Doc](src/plugin): IPlugin creator.hpp

--- src/Plugin/Creator/IPluginCreator.hpp
+++ src/Plugin/Creator/IPluginCreator.hpp
@@ -8,8 +8,19 @@ namespace Uniti {
     template<typename PLUGIN, typename PARENT>
     class IPluginCreator {
     public:
+        /** @brief Default virtual destructor for the IPluginCreator class.
+        */
         virtual ~IPluginCreator() = default;
+
+        /** @brief Pure virtual function to remove a PLUGIN element.
+            @param PLUGIN *element: A pointer to the PLUGIN element to remove.
+        */
         virtual void remove(PLUGIN *element) = 0;
+
+        /** @brief Pure virtual function to create a PLUGIN element.
+            @param PARENT &parent: Reference to the parent of the PLUGIN element.
+            @return PLUGIN *: Pointer to the newly created PLUGIN element.
+        */
         virtual PLUGIN *create(PARENT &parent) = 0;
     };
 }
