4c6043a5cc4f9ceaae4688dc6671f49a7b517bf0
swann <swann.lagoute@epitech.eu>
2023-11-05 16:56:22 +0100

    [Doc](plugin/render): add interafce doc

--- Plugins/Render/Includes/Event/IMouse.hpp
+++ Plugins/Render/Includes/Event/IMouse.hpp
@@ -7,28 +7,62 @@
 #include <string>
 #include "Vector2f.hpp"
 
+/** @brief Interface for handling mouse input.
+*/
 class IMouse {
 public:
+    /** @brief Enumeration of supported mouse buttons.
+    */
     enum KeyMouse {
         LEFT,
         RIGHT,
         MIDDLE
     };
 
+    /** @brief Virtual destructor for the IMouse class.
+    */
     virtual ~IMouse() = default;
 
+    /** @brief Update method to process and update mouse input state.
+    */
     virtual void update() = 0;
+
+    /** @brief Check if a specific mouse button (by string representation) is currently pressed.
+        @param const std::string &key: The key to check.
+        @return bool: True if the mouse button is currently pressed; otherwise, false.
+    */
     virtual bool isMousePressed(const std::string &key) = 0;
 
+    /** @brief Check if a specific mouse button is currently pressed.
+        @param KeyMouse keyMouse: The mouse button to check.
+        @return bool: True if the mouse button is currently pressed; otherwise, false.
+    */
     virtual bool isMousePressed(KeyMouse keyMouse) = 0;
 
+    /** @brief Check if a specific mouse button (by string representation) is currently released.
+        @param const std::string &key: The key to check.
+        @return bool: True if the mouse button is currently released; otherwise, false.
+    */
     virtual bool isMouseReleased(const std::string &key) = 0;
 
+    /** @brief Check if a specific mouse button is currently released.
+        @param KeyMouse keyMouse: The mouse button to check.
+        @return bool: True if the mouse button is currently released; otherwise, false.
+    */
     virtual bool isMouseReleased(KeyMouse keyMouse) = 0;
 
+    /** @brief Get the current mouse cursor position.
+        @return Uniti::Vector2f: A 2D vector representing the mouse cursor position.
+    */
     virtual Uniti::Vector2f getPosition() = 0;
 
+    /** @brief Get a reference to a list of mouse input events.
+        @return std::vector<std::string>&: Reference to the list of mouse input events.
+    */
     virtual std::vector<std::string> &getEvents() = 0;
 
+    /** @brief Clear the list of mouse input events.
+    */
     virtual void clearEvents() = 0;
 };
+
--- Plugins/Render/Includes/Printable/IPrintablePlugin.hpp
+++ Plugins/Render/Includes/Printable/IPrintablePlugin.hpp
@@ -7,11 +7,21 @@
 #include "IObjectPlugin.hpp"
 #include "IPrintable.hpp"
 
+/** @brief Interface for plugins that provide printable objects.
+*/
 class IPrintablePlugin : public Uniti::IObjectPlugin {
 public:
+    /** @brief Virtual destructor for the IPrintablePlugin interface.
+    */
     virtual ~IPrintablePlugin() = default;
 
+    /** @brief Get a constant reference to the printable object provided by the plugin.
+        @return const IPrintable&: A constant reference to the printable object.
+    */
     virtual const IPrintable &getPrintable() const = 0;
 
+    /** @brief Get a reference to the printable object provided by the plugin.
+        @return IPrintable&: A reference to the printable object.
+    */
     virtual IPrintable &getPrintable() = 0;
 };
